cced91d71779bfaeba69e9e2d14f9bf7b4fa5533
Kolby Kiernan <kolbykiernan@gmail.com>
2024-03-07 12:45:15 -0800

    created role-based authorization

--- backend/controllers/places.js
+++ backend/controllers/places.js
@@ -3,7 +3,11 @@ const db = require("../models")
 
 const { Place, Comment, User } = db
 
+
 router.post('/', async (req, res) => {
+    if(req.currentUser?.role !== 'admin'){
+        return res.status(403).json({ message: 'You are not allowed to add a place'})
+    }
     if (!req.body.pic) {
         req.body.pic = 'http://placekitten.com/400/400'
     }
@@ -45,6 +49,9 @@ router.get('/:placeId', async (req, res) => {
 })
 
 router.put('/:placeId', async (req, res) => {
+    if(req.currentUser?.role !== 'admin'){
+        return res.status(403).json({ message: 'You are not allowed to edit places'})
+    }
     let placeId = Number(req.params.placeId)
     if (isNaN(placeId)) {
         res.status(404).json({ message: `Invalid id "${placeId}"` })
@@ -63,6 +70,9 @@ router.put('/:placeId', async (req, res) => {
 })
 
 router.delete('/:placeId', async (req, res) => {
+    if(req.currentUser?.role !== 'admin'){
+        return res.status(403).json({ message: 'You are not allowed to delete places'})
+    }
     let placeId = Number(req.params.placeId)
     if (isNaN(placeId)) {
         res.status(404).json({ message: `Invalid id "${placeId}"` })
@@ -80,6 +90,9 @@ router.delete('/:placeId', async (req, res) => {
         }
     }
 })
+  
+
+  
 
 router.post('/:placeId/comments', async (req, res) => {
     const placeId = Number(req.params.placeId)
@@ -91,27 +104,26 @@ router.post('/:placeId/comments', async (req, res) => {
     })
 
     if (!place) {
-        res.status(404).json({ message: `Could not find place with id "${placeId}"` })
+        return res.status(404).json({ message: `Could not find place with id "${placeId}"` })
     }
 
-    const author = await User.findOne({
-        where: { userId: req.body.authorId }
-    })
-
-    if (!author) {
-        res.status(404).json({ message: `Could not find author with id "${req.body.authorId}"` })
+    if (!req.currentUser) {
+        return res.status(404).json({ message: `You must be logged in to leave a rant or rave.` })
     }
 
     const comment = await Comment.create({
         ...req.body,
+        authorId: req.currentUser.userId,
         placeId: placeId
     })
 
     res.send({
         ...comment.toJSON(),
-        author
+        author: req.currentUser
     })
 })
+
+
 
 router.delete('/:placeId/comments/:commentId', async (req, res) => {
     let placeId = Number(req.params.placeId)
--- frontend/src/places/NewCommentForm.js
+++ frontend/src/places/NewCommentForm.js
@@ -1,5 +1,5 @@
-import { useState, useEffect } from "react"
-import { useHistory } from "react-router"
+import { useState, useEffect, useContext } from "react"
+// import { useHistory } from "react-router"
 
 function NewCommentForm({ place, onSubmit }) {
 
@@ -22,9 +22,7 @@ function NewCommentForm({ place, onSubmit }) {
         fetchData()
     }, [])
 
-    let authorOptions = authors.map(author => {
-        return <option key={author.userId} value={author.userId}>{author.firstName} {author.lastName}</option>
-    })
+
 
     function handleSubmit(e) {
         e.preventDefault()
@@ -37,6 +35,14 @@ function NewCommentForm({ place, onSubmit }) {
         })
     }
 
+    // const { currentUser } = useContext(CurrentUser)
+
+    // if(!currentUser){
+    //     return <p>You must be logged in to leave a rant or rave.</p>
+    // }
+
+
+    
     return (
         <form onSubmit={handleSubmit}>
             <div className="row">
@@ -56,7 +62,7 @@ function NewCommentForm({ place, onSubmit }) {
                 <div className="form-group col-sm-4">
                     <label htmlFor="state">Author</label>
                     <select className="form-control" value={comment.authorId} onChange={e => setComment({ ...comment, authorId: e.target.value })}>
-                        {authorOptions}
+                        {/* {authorOptions} */}
                     </select>
                 </div>
                 <div className="form-group col-sm-4">
